611f23523d1fde7fa80786bfc5b7ef7619352192
LuismGil <luism6977@gmail.com>
2022-03-22 12:30:26 -0300

    adding initial configurations

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/global.scss';
+
+const app = () => {
+  return (
+    <h1>
+      Hello World, welcome to the React Course!
+    </h1>
+  );
+};
+
+export default app;
